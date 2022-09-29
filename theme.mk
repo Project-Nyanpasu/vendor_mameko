cf4742424ecc921f002fc966c5066ad90c9f987c
ReallySnow <reallysnow233@gmail.com>
2022-09-30 01:12:35 +0800

    mameko: Move theme overlay to their repo
    Change-Id: Ie40f4966b6ce73af95e00d4ad8bfa086a21e7978

---
+++ theme.mk
@@ -0,0 +1,34 @@
+#
+# Copyright (C) 2022 The Project Nyanpasu
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Font
+PRODUCT_PACKAGES += \
+    FontGoogleSansOverlay \
+    FontGoogleSansLatoOverlay \
+    FontInterOverlay \
+    FontOppoSansOverlay
+
+# google-sans family
+PRODUCT_PACKAGES += \
+    GoogleSans-Italic.ttf \
+    GoogleSans-Regular.ttf \
+    Inter-VF.ttf \
+    OppoSans-En-Regular.ttf
+
+PRODUCT_COPY_FILES += \
+    vendor/mameko/fonts/fonts_customization.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/fonts_customization.xml
+
+# Icon
+PRODUCT_PACKAGES += \
+    IconPackCircularAndroidOverlay \
+    IconPackCircularLauncherOverlay \
+    IconPackCircularSettingsOverlay \
+    IconPackCircularSystemUIOverlay
+
+# Packages
+PRODUCT_PACKAGES += \
+    NyanpasuWallpaperStub \
+    NyanpasuThemesStub
